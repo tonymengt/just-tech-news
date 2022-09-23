bbd8eccb78d6dbbb6d32f6ae385d8209c0fed9df
Tmeng <tonymengt@gmail.com>
2022-09-23 17:34:37 -0400

    added bcrypt and hooks to routes for passwords

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const {Model, DataTypes} = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 // create our User model
 class User extends Model {}
@@ -37,6 +38,21 @@ User.init(
         }
     },
     {
+        
+        hooks: {
+            // set up beforeCreate lifecycle "hook" functionality
+            async beforeCreate(newUserData){
+                newUserData.password = await bcrypt.hash(newUserData.password, 10) 
+                    return newUserData;
+                },
+            
+            async beforeUpdate(newUserData){
+                newUserData.password = await bcrypt.hash(newUserData.password, 10)
+                return newUserData
+            }
+
+            },
+        
         // table configuration optiones go here (https://sequelize.org/v5/manual/models-definition.html#configuration)
 
         // pass in our imported sequelize connection (the direct connection to our database)
